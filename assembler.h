37427fc00ac3fa789f25a57c3f057feff7e4210b
katefaiz <fem.e@mail.ru>
2025-10-24 16:46:49 +0300

    Add files via upload

--- assembler.h
+++ assembler.h
@@ -2,16 +2,16 @@
 #define AS_H
 
 #include "stack_funks.h" 
-#include "processor_struct_enum.h"
+#include "assembler_struct_enum.h"
 
-Processor_err assembler_init(Assembler *assembler);
-Processor_err assembler_compile(Assembler *assembler);
-Processor_err assembler_resolve_labels(Assembler *assembler);
-Processor_err assembler_save_to_file(Assembler *assembler);
-Processor_err assembler_destroy(Assembler *assembler);
+Assembler_err assembler_init(Assembler *assembler);
+Assembler_err assembler_compile(Assembler *assembler);
+Assembler_err assembler_resolve_labels(Assembler *assembler);
+Assembler_err assembler_save_to_file(Assembler *assembler);
+Assembler_err assembler_destroy(Assembler *assembler);
 Registers_name comparing_registers(const char *reg_name);
 Commands comparing_commands(const char *command);
-Processor_err disasm_commands_data(int program[]);
+Assembler_err disasm_commands_data(int program[]);
 //void stack_calculate (Stack_t *stk, char *command, int value);
 
 #endif //AS_H
